fdaf2684bbbfe304a76474eb64c10408f1457bbe
sadrumsumi <michaelkilongoo193@gmail.com>
2022-10-05 17:24:47 +0300

    firebase initilzation

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+function Header() {
+  return (
+    <Container>
+    <Navbar expand="lg" variant="dark" bg="dark">
+      <Container>
+        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+      </Container>
+    </Navbar>
+  </Container>
+  )
+}
+
+export default Header
